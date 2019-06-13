c56948f04cdbe3f5b48fb2fda18b254e88924587
anisidrenmouche <anisidrenmouche@hotmail.fr>
2019-06-13 14:02:36 +0200

    creation entity+ repository

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -24,16 +24,20 @@ class BlogController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home(){
+    public function home()
+    {
         return $this->render('blog/home.html.twig');
     }
 
-     /**
+    /**
      * @Route("/blog/1", name="blog_show")
      */
-     public function show(){
-       return $this->render('blog/show.html.twig');
+
+    public function show()
+    {
+        return $this->render('blog/show.html.twig');
     }
-    }
+}
+
 
 
