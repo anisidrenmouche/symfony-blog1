dc9ff60fd2270d4e3bea5d1b3afc7ad646a1766b
anisidrenmouche <anisidrenmouche@hotmail.fr>
2019-06-15 11:34:41 +0200

    correction des fictures + img

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -14,7 +14,7 @@ class ArticleFixtures extends Fixture
             $article = new Article();
             $article->setTitle("Le titre des artciles n°$i")
                 ->setContent("<p>Contenu de l'article n°$i </p>")
-                ->setImage("")
+                ->setImage("https://images.gtsstatic.net/reno/imagereader.aspx?imageurl=http%3A%2F%2Fm.sothebysrealty.com%2F1103i215%2Ftts21f1s904p4da22y7jcv9s93i215&option=N&idclient=180&idlisting=180-l-3136-lxqp5n&w=1024&permitphotoenlargement=false")
                 ->setCreatedAt(new \DateTime());
             $manager->persist($article);
         }
