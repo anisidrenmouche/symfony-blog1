a5380912491fd293dcfd86a7cb2a1d1cf789d924
anisidrenmouche <anisidrenmouche@hotmail.fr>
2019-06-18 10:30:40 +0200

    injection des dependances

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
@@ -11,7 +12,7 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
         $repo = $this->getDoctrine()->getRepository(Article::class);
         $articles = $repo->findAll();
@@ -38,12 +39,11 @@ class BlogController extends AbstractController
      * @Route("/blog/{id}", name="blog_show")
      */
 
-    public function show($id)
+    public function show(Article $article)
     {
 
         $repo = $this->getDoctrine()->getRepository(Article::class);
 
-        $article = $repo->find($id);
 
         return $this->render('blog/show.html.twig',[
             'article' => $article
